ad2d632b28058a2a77e1e637898753a749ca3ed4
Sudarsanan P R <sudarsanan@sociusus.com>
2026-04-07 15:27:37 +0530

    Added Barcode Auto ceate logic

--- product_addons/models/product_template.py
+++ product_addons/models/product_template.py
@@ -6,3 +6,14 @@ class ProductTemplate(models.Model):
     product_color = fields.Char(string='Product Color')
     product_size = fields.Char(string='Product Size')
     upc_code = fields.Char(string='UPC Code')
+    
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+    
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        for val in vals_list:
+            if not val.get('barcode'):
+                val['barcode'] = self.env['ir.sequence'].next_by_code('product.barcode')
+        return super(ProductProduct, self).create(vals_list)
