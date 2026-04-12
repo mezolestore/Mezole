007e2222595d9f4476bb8a2a9a64fe9755829873
Sudarsanan P R <sudarsanan@sociusus.com>
2026-04-12 13:02:26 +0530

    Adde MRP in template and barcode

--- product_addons/models/product_template.py
+++ product_addons/models/product_template.py
@@ -6,14 +6,27 @@ class ProductTemplate(models.Model):
     product_color = fields.Char(string='Product Color')
     product_size = fields.Char(string='Product Size')
     upc_code = fields.Char(string='UPC Code')
-    
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
-    
+    mrp_price = fields.Float(string='MRP Price')
     
     @api.model_create_multi
     def create(self, vals_list):
         for val in vals_list:
+            if not val.get('barcode'):
+                val['barcode'] = self.env['ir.sequence'].next_by_code('product.barcode')
+        return super(ProductTemplate, self).create(vals_list)
+
+    
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+    
+    mrp_price = fields.Float(string='MRP Price')
+    @api.model_create_multi
+    def create(self, vals_list):
+        for val in vals_list:
+            if val.get('product_tmpl_id'):
+                template = self.env['product.template'].browse(val['product_tmpl_id'])
+                if template.barcode:
+                    val['barcode'] = template.barcode
             if not val.get('barcode'):
                 val['barcode'] = self.env['ir.sequence'].next_by_code('product.barcode')
         return super(ProductProduct, self).create(vals_list)
