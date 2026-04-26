c0c164cd9282356550d9b685fb22ccfc6b9d53c9
Sudarsanan P R <sudarsanan@sociusus.com>
2026-04-26 15:46:34 +0530

    added data remove module

---
+++ om_data_remove/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Odoo 18 Remove Data',
+    'version': '18.0.0.0.1',
+    'author': 'Odoo Mates, Sunpop.cn',
+    'category': 'Tools',
+    'description': 'Data Clean up, Remove Data, Database Clean UP, Reset Database',
+    'summary': 'Data Clean up, Remove Data, Database Clean UP, Reset Database',
+    'maintainer': 'Odoo Mates',
+    'support': 'odoomates@gmail.com',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        'views/view.xml',
+    ],
+    'images': ['static/description/banner.png'],
+}
