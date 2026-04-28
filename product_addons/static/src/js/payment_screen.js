61777ffd1fed5ded5ce8a7cf1507663006a77c17
Sudarsanan P R <sudarsanan@sociusus.com>
2026-04-28 23:18:36 +0530

    Category updated

--- product_addons/static/src/js/payment_screen.js
+++ product_addons/static/src/js/payment_screen.js
@@ -3,6 +3,8 @@
 import { patch } from "@web/core/utils/patch";
 import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
 import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+import { PosOrderline } from "@point_of_sale/app/models/pos_order_line";
+import { Orderline } from "@point_of_sale/app/generic_components/orderline/orderline";
 
 patch(PaymentScreen.prototype, {
 
@@ -18,5 +20,36 @@ patch(PaymentScreen.prototype, {
         }
 
         await super.validateOrder(...arguments);
+    },
+});
+
+patch(PosOrderline.prototype, {
+
+    getDisplayData() {
+        return {
+            ...super.getDisplayData(...arguments),
+            category: this.product_id.categ_id.name,
+        };
+    },
+
+    // EXTENDS 'point_of_sale'
+    prepareBaseLineForTaxesComputationExtraValues(customValues = {}) {
+        const extraValues = super.prepareBaseLineForTaxesComputationExtraValues(customValues);
+        extraValues.category = this.product_id.categ_id.name;
+        return extraValues;
+    },
+    
+});
+
+patch(Orderline, {
+    props: {
+        ...Orderline.props,
+        line: {
+            ...Orderline.props.line,
+            shape: {
+                ...Orderline.props.line.shape,
+                category: { type: String, optional: true },
+            },
+        },
     },
 });
