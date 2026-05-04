8f4f2a51caf926476f8d5fc3df1ff80d12652ed5
Sudarsanan P R <sudarsanan@sociusus.com>
2026-05-04 11:33:04 +0530

    Changed abv calculation

--- product_addons/models/pos_order.py
+++ product_addons/models/pos_order.py
@@ -3,16 +3,14 @@ from odoo import models, fields, api
 class PosOrder(models.Model):
     _inherit = 'pos.order'
     
-    a_b_value = fields.Float(string='Average Bill Value', compute='_compute_a_b_value',compute_sudo=True, store=True, aggregator="avg")
-    upt_value = fields.Float(string='Unit Per Transaction', compute='_compute_upt_value',compute_sudo=True, store=True, aggregator="avg")
+    abvalue = fields.Float(string='Average Bill Value', compute='_compute_a_b_value', compute_sudo=True, store=True, aggregator="avg")
+    upt_value = fields.Float(string='Unit Per Transaction', compute='_compute_upt_value', compute_sudo=True, store=True, aggregator="avg")
     
-    @api.depends('lines')
+    @api.depends('lines.price_subtotal_incl')
     def _compute_a_b_value(self):
         for move in self:
-            total_amount = sum(line.price_subtotal_incl for line in move.lines)
-            total_quantity = sum(line.qty for line in move.lines)
-            move.a_b_value = total_amount / total_quantity if total_quantity > 0 else 0.0
-            
+            move.abvalue = sum(move.lines.mapped('price_subtotal_incl'))
+
     @api.depends('lines')
     def _compute_upt_value(self):
         for move in self:
